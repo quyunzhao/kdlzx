91c4cbb1f61d36a15ea5d9c3c8e6b6a41232946c
jingqu <1264352733@qq.com>
2020-12-13 20:08:50 +0800

    图片组件

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class FormInput extends Component {
+  render() {
+    return <div>sssssss</div>;
+  }
+}
+
+export default FormInput;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "../assets/styles/loginPage.less";
 import Img from "../components/Img";
 
+import FormInput from "../components/FormInput";
+
 // const Img = require("../assets/images/logo192.png");
 // console.log(Img);
 export class LoginPage extends Component {
@@ -11,6 +13,11 @@ export class LoginPage extends Component {
         {/* 1 logo 图片 */}
         <Img src="logo.png"></Img>
         {/* form 表单 */}
+        <form action="#" className="login-form">
+          {/* 用户名输入框 */}
+          <FormInput></FormInput>
+          {/* 密码输入框 */}
+        </form>
       </div>
     );
   }
