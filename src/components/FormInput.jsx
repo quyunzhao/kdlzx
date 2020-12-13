f272fc5bf746b99b9ab5683e420a349905c3b45b
jingqu <1264352733@qq.com>
2020-12-13 20:40:25 +0800

    引入图标字体

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "../assets/styles/FormInput.less";
+import "../assets/fonts/iconfont.css";
 export class FormInput extends Component {
   render() {
     return (
       <div className="input-group">
         {/* 字体图标 */}
-        <i></i>
+        <i className="iconfont icon-shouji"></i>
         {/* 输入框 */}
         <input type="text" />
       </div>
