c4f4e6206c55dd8a19061c735c3527caa3be3981
jingqu <1264352733@qq.com>
2020-12-18 20:08:56 +0800

    subject

--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -17,40 +17,40 @@ export default class Subject extends Component {
           </Flex.Item>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 1 }}></i>
+              <p>全栈UX设计</p>
             </a>
           </Flex.Item>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 2 }}></i>
+              <p>H5前端</p>
             </a>
           </Flex.Item>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 3 }}></i>
+              <p>Python</p>
             </a>
           </Flex.Item>
         </Flex>
         <Flex>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 4 }}></i>
+              <p>ios</p>
             </a>
           </Flex.Item>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 5 }}></i>
+              <p>大数据</p>
             </a>
           </Flex.Item>
           <Flex.Item>
             <a href="###">
-              <i></i>
-              <p>Java EE</p>
+              <i style={{ backgroundPositionX: -50 * 6 }}></i>
+              <p>c++</p>
             </a>
           </Flex.Item>
           <Flex.Item></Flex.Item>
