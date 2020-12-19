5cad21c3b8635c498a737f6befeafee0292d0f91
jingqu <1264352733@qq.com>
2020-12-19 12:00:50 +0800

    tab 栏完成

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,29 +11,29 @@ import Subject from "../components/Subject";
 
 const sub_list_data = [
   {
-    img: "1.gif",
+    img: "bpic22021_s.jpg",
     title: "基础教程",
     des: "Python 是一种解释型、面向对象、动态数据类型的高级程序设计语言。",
   },
   {
-    img: "2.gif",
+    img: "apic29680.jpg",
     title: "发展历史",
     des:
       "Python 是由 Guido van Rossum 在八十年代末和九十年代初，在荷兰国家数学和计算机科学研究所设计出来的。",
   },
   {
-    img: "3.gif",
+    img: "bpic22018.jpg",
     title: "环境搭建",
     des: "本章节我们将向大家介绍如何在本地搭建Python开发环境。",
   },
   {
-    img: "4.gif",
+    img: "hpic3289.jpg",
     title: "中文编码",
     des:
       "前面章节中我们已经学会了如何用 Python 输出 Hello, World!，英文没有问题，但是如果你输出中文字符 就有可能会碰到中文编码问题。",
   },
   {
-    img: "5.gif",
+    img: "bpic22018.jpg",
     title: "基础语法",
     des:
       "Python 语言与 Perl，C 和 Java 等语言有许多相似之处。但是，也存在一些差异。",
